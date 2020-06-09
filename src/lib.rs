3a8be7b9ae081b0d3550d4904770874436a49c85
Abhilash Meesala <ameesala@salesforce.com>
2020-06-09 19:01:20 +0530

    Factor out FrequencyMap in to map module and add tests
    FrequencyMap and it's related operations (FrequencyMapOps and RandomMap)
    makes more sense as a separate module than as part of the lib.

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,9 @@
 use std::collections::HashMap;
-use std::hash::Hash;
-
-use rand::{Rng, thread_rng};
+mod map;
+use map::*;
 
 type Token = Option<char>;
 type Ngram = Vec<Token>;
-type FrequencyMap<T> = HashMap<T, usize>;
-
-trait FrequencyMapOps<T> {
-    fn incr(&mut self, key: T);
-    fn count(&self) -> usize;
-}
-
-impl<T: Eq + Hash> FrequencyMapOps<T> for FrequencyMap<T> {
-    fn incr(&mut self, key: T) {
-        let count = self.entry(key).or_insert(0);
-        *count += 1;
-    }
-
-    fn count(&self) -> usize {
-        self.values().sum()
-    }
-}
-
-trait MapRandom<T> {
-    fn choose_weighted(&self) -> &T;
-}
-
-impl<T> MapRandom<T> for FrequencyMap<T> {
-    fn choose_weighted(&self) -> &T {
-        let mut rng = thread_rng();
-        let total_tokens: usize = self.values().sum();
-        let chosen_token = rng.gen_range(0, total_tokens + 1);
-        let mut sum = 0;
-        for (key, value) in self.iter() {
-            sum += *value;
-            if sum >= chosen_token {
-                return key;
-            }
-        }
-        //FIXME: this is recheable if order is say 100
-        unreachable!()
-    }
-}
 
 pub struct NameGenerator {
     prefix_ngrams: FrequencyMap<Ngram>,
@@ -95,10 +56,10 @@ impl NameGenerator {
     }
 
     pub fn generate(&self) -> String {
-        let mut ngram = self.prefix_ngrams.choose_weighted().clone();
+        let mut ngram = self.prefix_ngrams.choose_weighted().ok().unwrap().clone();
         let mut chars: Vec<char> = ngram.iter().flatten().copied().collect();
         while let Some(next_states) = self.state_map.get(&ngram) {
-            if let Some(c) = next_states.choose_weighted() {
+            if let Some(c) = next_states.choose_weighted().ok().unwrap() {
                 ngram = ngram[1..].to_vec();
                 ngram.push(Some(*c));
                 chars.push(*c);
---
+++ src/map.rs
@@ -0,0 +1,117 @@
+use std::collections::HashMap;
+use std::hash::Hash;
+use rand::{Rng, thread_rng};
+
+pub type FrequencyMap<T> = HashMap<T, usize>;
+
+pub trait Hashable: Eq + Hash {}
+
+impl<T> Hashable for T where T: Eq + Hash {}
+
+pub(crate) trait FrequencyMapOps<T: Hashable> {
+    fn incr(&mut self, key: T);
+    fn count(&self) -> usize;
+}
+
+impl<T> FrequencyMapOps<T> for FrequencyMap<T> where T: Hashable {
+    fn incr(&mut self, key: T) {
+        let count = self.entry(key).or_insert(0);
+        *count += 1;
+    }
+
+    fn count(&self) -> usize {
+        self.values().sum()
+    }
+}
+
+pub trait MapRandom<T> {
+    fn choose_weighted(&self) -> MapRandomResult<&T>;
+}
+
+#[derive(Debug, PartialEq)]
+pub enum MapRandomError {
+    EmptyMap,
+}
+
+pub type MapRandomResult<T> = Result<T, MapRandomError>;
+
+impl<T> MapRandom<T> for FrequencyMap<T> where T: Hashable{
+    fn choose_weighted(&self) -> MapRandomResult<&T> {
+        let mut rng = thread_rng();
+        let total_tokens: usize = self.count();
+
+        if self.len() == 0 {
+            return Err(MapRandomError::EmptyMap);
+        }
+
+        let chosen_token = rng.gen_range(0, total_tokens + 1);
+        let mut sum = 0;
+        for (key, value) in self.iter() {
+            sum += *value;
+            if sum >= chosen_token {
+                return Ok(key);
+            }
+        }
+        unreachable!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_incr() {
+        let mut map = FrequencyMap::new();
+
+        // it should add if key does not exist
+        assert!(!map.contains_key("a"));
+        map.incr("a");
+        assert_eq!(map.get("a"), Some(&1));
+
+        // it should increment if key exists
+        map.incr("a");
+        assert_eq!(map.get("a"), Some(&2));
+
+        // and keep on incrementing...
+        for _ in 0..10 {
+            map.incr("a");
+        }
+        assert_eq!(map.get("a"), Some(&12));
+
+        // it should not have recollection of previous value
+        map.remove("a");
+        map.incr("a");
+        assert_eq!(map.get("a"), Some(&1));
+    }
+
+    #[test]
+    fn test_count() {
+        let mut map = FrequencyMap::new();
+
+        // empty map's count is 0
+        assert_eq!(map.count(), 0);
+
+        // insert first 10 natural numbers as values
+        // set keys to something different to differentiate
+        // that its summing values not keys
+        for (k, v) in (101..=110).zip(1..=10) {
+            map.insert(k,v);
+        }
+        assert_eq!(map.count(), 55)
+    }
+
+    #[test]
+    fn test_choose_weighted() {
+        // Err on empty map
+        let map: FrequencyMap<&str> = FrequencyMap::new();
+        assert_eq!(map.choose_weighted().err(), Some(MapRandomError::EmptyMap));
+
+        // Otherwise return a value.
+        let map: FrequencyMap<&str> = vec![
+            ("one", 1),
+            ("two", 2)
+        ].into_iter().collect();
+        assert!(map.choose_weighted().is_ok());
+    }
+}
